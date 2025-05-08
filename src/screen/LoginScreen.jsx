de634c8387970b41ae558904e741e84fb88b2ac2
Kalos007 <daviskalokola@gmail.com>
2025-05-09 02:48:42 +0300

    fourth commit

--- src/screen/LoginScreen.jsx
+++ src/screen/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text,TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text,TextInput,TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { color } from '../utils/color'
@@ -12,7 +12,30 @@ const LoginScreen = () => {
         <TouchableOpacity style={styles.backButtonWrapper} onPress={() => navigation.goBack(HomeScreen)}>
             <Ionicons name="arrow-back" size={40} color="White"/>
         </TouchableOpacity>
-        <Text style={{fontSize:30,marginTop:50,alignItems:"center"}}>Login</Text>
+     <View style={styles.containerHey}> 
+        <Text style={styles.headingText}>Hey,</Text>
+        <Text style={styles.headingText}>Welcome</Text>
+        <Text style={styles.headingText}>Back</Text>
+     </View>
+        <View style={styles.formContainer}>
+            <View style={styles.inputContainer}>
+             <Ionicons name={"mail-outline"} size={30} color={"secondary"} />
+             <TextInput
+              style={styles.input}
+              placeholder="Enter your Email"
+             placeholderTextColor="#999"
+            />
+            </View>
+            <View style={styles.inputContainer}>
+             <Ionicons name={"Password-outline"} size={30} color={"secondary"} />
+             <TextInput
+              style={styles.input}
+              placeholder="Enter your Password"
+              secureTextEntry={true}
+             placeholderTextColor="#999"
+            />
+            </View>
+     </View>
     </View>
   )
 }
@@ -36,7 +59,39 @@ const styles = StyleSheet.create({
         alignItems:"center",
         marginTop: 30,
         marginLeft: 30,
+    },
+    containerHey:{
+        marginTop: 30,
+        marginLeft: 30,
+    },
+    headingText:{
+        fontSize: 30,
+        color: "black",
+        fontWeight: "bold",
+    },
+    formContainer:{
+        marginTop: 30,
+        marginLeft: 30,
+    },
+    inputContainer:{
+        height: 50,
+        width: "90%",
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderColor: "secondary",
+        borderRadius: 100,
+        alignItems: "center",
+        flexDirection:"row",
+        paddingHorizontal:20,
+        alignItems:"center",
+        marginVertical: 15,        
+    },
+    input:{
+        flex:1,
+        paddingHorizontal: 10,
+        fontSize: 16,
     }
-    
+
+
 
 })
