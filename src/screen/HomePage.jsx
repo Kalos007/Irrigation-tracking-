8042861881e3957049851e15ffc2efb8862a37a1
Kalos007 <daviskalokola@gmail.com>
2025-05-09 05:20:22 +0300

    modifying homepage

--- src/screen/HomePage.jsx
+++ src/screen/HomePage.jsx
@@ -1,14 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, ImageBackground,Text, View } from 'react-native'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <View>
-      <Text>HomePage</Text>
+    <View style={styles.container}>
+
+      
+      
+      <View style={styles.containerpatch}>
+      
+        
+      </View>
+      
     </View>
   )
-}
+};
 
 export default HomePage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        //marginBottom: 30,
+        //marginTop: 30,
+
+    },
+    containerpatch: {
+        width: '90%',
+        height: 200,
+        backgroundColor: 'teal',
+        //borderWidth: 1,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30,
+        
+    },
+    greenleaf: {
+        width: '100%',  // or specific width like 300
+        height: '100%',    // set your desired height
+        justifyContent: 'center', // centers text vertically
+        alignItems: 'center',     // centers text horizontally
+        borderRadius: 50,
+        borderWidth: 0,
+        borderColor: "",
+         // use your color variable
+        //overflow: 'hidden',       // needed for borderRadius to work
+    },
+
+})
--- src/screen/SignupScreen.jsx
+++ src/screen/SignupScreen.jsx
@@ -1,19 +1,67 @@
-import { StyleSheet, Text, View ,TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View ,TextInput,TouchableOpacity } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import{ useNavigation } from '@react-navigation/native'
 import HomeScreen from './HomeScreen'
 import { color } from '../utils/color'
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
+import { useState ,React} from 'react'
 
 const SignupScreen = () => {
  const navigation = useNavigation();
+ const [secureEntery,setSecureEntery]=useState(true);
    return (
      <View style={styles.container}>
          <TouchableOpacity style={styles.backButtonWrapper} onPress={() => navigation.goBack(HomeScreen)}>
              <Ionicons name="arrow-back" size={40} color="White"/>
          </TouchableOpacity>
-         <Text style={{fontSize:30,marginTop:50,alignItems:"center"}}>Signup</Text>
+          <View style={styles.containerHey}> 
+                 <Text style={styles.headingText}>Hey,</Text>
+                 <Text style={styles.headingText}>Welcome</Text>
+                 <Text style={styles.headingText}>Back</Text>
+             </View>
+         <View style={styles.formContainer}>
+                     <View style={styles.inputContainer}>
+                      <Ionicons name={"mail-outline"} size={30} color={"secondary"} />
+                      <TextInput
+                       style={styles.input}
+                       placeholder="Enter your Email"
+                      placeholderTextColor="#999"
+                     />
+                     </View>
+                     <View style={styles.inputContainer}>
+                      <Ionicons name={"Password-outline"} size={30} color={"secondary"} />
+                      <TextInput
+                       style={styles.input}
+                       placeholder="Enter your Password"
+                       secureTextEntry={secureEntery}
+                      placeholderTextColor="#999"
+                     />
+                     <TouchableOpacity onPress={() => setSecureEntery((prev)=> !prev)}>
+                      <SimpleLineIcons name={"eye"} size={30} color={"secondary"} />
+                      </TouchableOpacity>
+                     </View>
+                     <View style={styles.inputContainer}>
+                      <Ionicons name={"Password-outline"} size={30} color={"secondary"} />
+                      <TextInput
+                       style={styles.input}
+                       placeholder="Confirm your Password"
+
+                       secureTextEntry={secureEntery}
+                      placeholderTextColor="#999"
+                     />
+                     <TouchableOpacity onPress={() => setSecureEntery((prev)=> !prev)}>
+                      <SimpleLineIcons name={"eye"} size={30} color={"secondary"} />
+                      </TouchableOpacity>
+                     </View>
+
+              </View>
+              <View style={styles.SignupContainer}>
+                 <TouchableOpacity style={styles.confirmSignup} onPress={() => navigation.navigate("Login")}>
+                     <Text style={styles.wordSignUp}>SignUp</Text>
+                 </TouchableOpacity>
+                 </View>
      </View>
+     
    )
  };
  
@@ -37,7 +85,64 @@ const styles = StyleSheet.create({
         alignItems:"center",
         marginTop: 30,
         marginLeft: 30,
-    }
+    },
+    headingText:{
+        fontSize: 30,
+        color: "black",
+        fontWeight: "bold",
+        marginTop: 10,
+        marginLeft: 30,
+    },
+    formContainer:{
+        marginTop: 30,
+        marginLeft: 30,
+    },
+    inputContainer:{
+        height: 50,
+        width: "90%",
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderColor: "secondary",
+        borderRadius: 100,
+        alignItems: "center",
+        flexDirection:"row",
+        paddingHorizontal:20,
+        marginVertical: 10,        
+    },
+    input:{
+        flex:1,
+        paddingHorizontal: 10,
+        fontSize: 16,
+
+    },
+    SignupContainer:{
+        flexDirection: 'row',
+        borderWidth:3,
+        height:50,
+        width: '82%',
+        borderColor: 'black',
+        borderRadius: 100 ,
+        marginTop: 28,
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    confirmSignup:{
+        backgroundColor: 'black', // Tomato
+        padding: 10,
+        borderRadius:98,
+        marginRight: 10,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 0,
+    },
+    wordSignUp:{
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    
     
 
 })
